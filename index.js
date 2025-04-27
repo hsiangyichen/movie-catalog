65e2e2fd5f667d0b59fc06df332d7253ebedab8c
hsiangyichen <hsiangyi1025@gmail.com>
2025-04-26 20:18:36 -0700

    feat: add Redis publishing functionality with /publish API endpoint

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 import express from "express";
+import { createClient } from "redis";
 import movieRoute from "./routes/movieRoutes.js";
 
 const app = express();
 const PORT = 8080;
 
+// Create Redis client
+const publisher = createClient();
+
 app.use(express.json());
 
+// Connect to Redis
+async function connectRedis() {
+  await publisher.connect();
+}
+connectRedis().catch(console.error);
+
+// Publish message API
+app.post("/publish", async (req, res) => {
+  const { channel, message } = req.body;
+  await publisher.publish(channel, message);
+  res.send(`Message has been published to channel: ${channel}`);
+});
+
+// Test homepage
 app.get("/", (req, res) => {
   res.send("Welcome to the Movie Catalog Service!");
 });
@@ -13,5 +31,5 @@ app.get("/", (req, res) => {
 app.use("/movies", movieRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
